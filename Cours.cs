14c8a4fd7d80c19f982188b4c515e98318c8ddc8
allaouaboucheneb01 <2695953@collegelacite.ca>
2021-04-16 13:34:54 -0400

    Ajout de la class Cours

---
+++ Cours.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Cours
+{
+	public Cours()
+	{
+		public int numeroDuCours = 0;
+		public int code = 0;
+		public String titre;
+
+		public int NumeroDuCours { get; set; }
+		public int Code { get; set; }
+		public String Titre { get; set; }
+	}
+}
